35265ad9ab1f1eb491529635a367d8ccf4aff605
vr <166162@gmail.com>
2017-04-15 15:36:41 +0300

    Fix scope issues and default props.

--- index.jsx
+++ index.jsx
@@ -3,25 +3,29 @@ import React from 'react';
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.defaultProps = {
-      src: 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=',
-      anchorX: '50%',
-      anchorY: '50%'
+    this.createStyle = this.createStyle.bind(this);
+  }
+
+  createStyle() {
+    return {
+      backgroundPosition: `${this.props.anchorX} ${this.props.anchorY}`,
+      backgroundImage: `url(${this.props.src}`
     }
   }
-  
-  createStyle() {
-    return `background-position:${this.props.anchorX} ${this.props.anchorY};
-      background-image:url(${this.props.src})`;
-  }
-  
+
   render() {
     return (
       <header style={ this.createStyle() }>
-        { props.children }
+        { this.props.children }
       </header>
     );
   }
 }
+
+Header.defaultProps = {
+  src: 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=',
+  anchorX: '50%',
+  anchorY: '50%'
+};
 
 export default Header;
